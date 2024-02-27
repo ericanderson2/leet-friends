bc409777d983d6e315b8e7d7f15e44ec355f26f5
eanderson <717eanderson@gmail.com>
2024-02-27 17:31:28 -0500

    added code to send submission notifications

--- scripts/background.js
+++ scripts/background.js
@@ -6,3 +6,32 @@ browser.runtime.onMessage.addListener(
     return true;
     }
 );
+
+function check_last_submission(username) {
+  let last_time = 0;
+  let url = `https://leetcode.com/graphql/?query=query{
+  recentSubmissionList(username: "${username}", limit: 1) {
+      title
+      titleSlug
+      timestamp
+      statusDisplay
+      lang
+  }
+}`;
+  fetch(url).then(response => response.json()).then(data => {
+    if (data["data"]["recentSubmissionList"].length == 0) {
+      return
+    }
+    let submission = data["data"]["recentSubmissionList"][0];
+    if (submission["timestamp"] != last_time) {
+      let content = `${submission["title"]} (${submission["statusDisplay"]})`;
+      let title = `${username} made a submission`
+      browser.notifications.create({
+        type: "basic",
+        iconUrl: "../images/lf_logo.png",
+        title,
+        message: content,
+      });
+    }
+  });
+}
