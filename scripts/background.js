5d0509f07bcfaf7894bcc63b8d17b909b215d138
eanderson <717eanderson@gmail.com>
2024-03-05 13:29:42 -0500

    alarms -> runtime and get question num for notifications

--- scripts/background.js
+++ scripts/background.js
@@ -1,20 +1,7 @@
 "use strict";
 
-let last_submission_time = {}
-
-browser.alarms.create("keep-loaded-alarm", {
-  periodInMinutes: 1
-});
-setTimeout(() => browser.alarms.create("keep-loaded-alarm-1", {
-  periodInMinutes: 1
-}), "20000");
-setTimeout(() => browser.alarms.create("keep-loaded-alarm-2", {
-  periodInMinutes: 1
-}), "40000");
-
-browser.alarms.onAlarm.addListener(() => {
-  console.log("keeping extension alive - log for debug");
-});
+let last_submission_time = {};
+let question_cache = {};
 
 browser.runtime.onMessage.addListener(
   function(url, sender, onSuccess) {
@@ -27,51 +14,60 @@ browser.notifications.onClicked.addListener((id, index) => {
   browser.tabs.create({
     url: "https://leetcode.com/" + id,
   });
-})
+});
 
-get_friend_updates()
+keep_alive();
+get_friend_updates();
 
-function check_last_submission(username, aliases) {
-  let url = `https://leetcode.com/graphql/?query=query{
-  recentSubmissionList(username: "${username}", limit: 1) {
-      title
-      titleSlug
-      timestamp
-      statusDisplay
-      lang
-  }
-}`;
-  let last_time = last_submission_time[username] ?? -1;
-
-  fetch(url).then(response =>response.json()).then(data => {
-    if (data["data"]["recentSubmissionList"].length == 0) {
-      return
-    }
-    let submission = data["data"]["recentSubmissionList"][0];
-    if (submission["timestamp"] != last_time) {
-      if (last_time > -1) {
-        let content = `${submission["title"]} (${submission["statusDisplay"]})`;
-        let title = `${username} made a submission`;
-        if (username in aliases) {
-          title = `${aliases[username]} (${username}) made a submission`;
-        }
-        browser.notifications.create(username, {
-          type: "basic",
-          iconUrl: "../images/lf_logo.png",
-          title,
-          message: content,
-        });
-      } else {
-        console.log("RESTARTED");
-      }
-
-      last_submission_time[username] = submission["timestamp"];
-    }
-  });
+function keep_alive() {
+  let getting = browser.runtime.getPlatformInfo();
+  setTimeout(keep_alive, "20000");
 }
 
+// Check if a user has made a recent submission, and send a notification if so
+async function check_last_submission(username, aliases) {
+  let url = `https://leetcode.com/graphql/?query=query{
+    recentSubmissionList(username: "${username}", limit: 1) {
+        title
+        titleSlug
+        timestamp
+        statusDisplay
+        lang
+    }
+  }`;
+
+  let response = await fetch(url);
+  let json = await response.json();
+
+  if (json["data"]["recentSubmissionList"].length == 0) {
+    return;
+  }
+
+  let submission = json["data"]["recentSubmissionList"][0];
+  let last_time = last_submission_time[username] ?? -1;
+
+  if (submission["timestamp"] != last_time) {
+    if (last_time > -1) {
+      let id = await get_question_num(submission["titleSlug"]);
+
+      let content = `${id}. ${submission["title"]} (${submission["statusDisplay"]})`;
+      let title = `${username} made a submission`;
+      if (username in aliases) {
+        title = `${aliases[username]} (${username}) made a submission`;
+      }
+      browser.notifications.create(username, {
+        type: "basic",
+        iconUrl: "../images/lf_logo.png",
+        title,
+        message: content,
+      });
+    } // else: restarted script or new user in watchlist
+    last_submission_time[username] = submission["timestamp"];
+  }
+}
+
+// Check if any friends on the watchlist have made a recent submission
 function get_friend_updates() {
-  console.log("polling for updateas");
   let aliases = {};
   browser.storage.sync.get("aliases").then(res => {
     aliases = res.aliases || {};
@@ -84,4 +80,25 @@ function get_friend_updates() {
   }));
 
   setTimeout(get_friend_updates, "30000");
+}
+
+// Return the frontend id of the given question
+async function get_question_num(title_slug) {
+  if (title_slug in question_cache) {
+    return question_cache[title_slug];
+  }
+
+  let url = `https://leetcode.com/graphql/?query=query{
+    question(titleSlug: "${title_slug}") {
+        questionId
+        questionFrontendId
+    }
+  }`;
+  let response = await fetch(url);
+  let json = await response.json();
+
+  let id = json["data"]["question"]["questionFrontendId"];
+  question_cache[title_slug] = id;
+
+  return id;
 }
